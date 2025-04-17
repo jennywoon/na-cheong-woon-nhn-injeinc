a9a1bc29509cca401b0fdd5139107536433cd5e2
jennywoon <109018926+jennywoon@users.noreply.github.com>
2025-04-17 19:45:15 +0900

    add: 'npm run serve' 명령을 통해 파일이 되는 HMR 을 위한 셋팅 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/index.js',
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
-    mode: 'development'
+    mode: 'development',
+    devtool: 'inline-source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        hot: true,
+        open: true,
+    }
 };
