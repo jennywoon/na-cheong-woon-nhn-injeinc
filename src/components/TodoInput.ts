6407b47fea4ae270ee27b2fff96201283deba81f
jennywoon <109018926+jennywoon@users.noreply.github.com>
2025-04-17 23:54:29 +0900

    style: 콘솔에 todo 등록 메시지 추가

--- src/components/TodoInput.ts
+++ src/components/TodoInput.ts
@@ -22,6 +22,7 @@ export function createTodoInput(onSubmitTodo: (value: string) => void): HTMLInpu
             if(value){
                 onSubmitTodo(value);
                 input.value = '';
+                console.log('todo 등록');
             };
         };
     });
