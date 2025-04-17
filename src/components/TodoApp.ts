7168e9fb5b5e905c0d53ba68f5e1b8d05deb8450
jennywoon <109018926+jennywoon@users.noreply.github.com>
2025-04-17 22:13:04 +0900

    feat: 등록된 Todo 아이템 Todo 목록 상단 추가

--- src/components/TodoApp.ts
+++ src/components/TodoApp.ts
@@ -2,15 +2,27 @@ import { createTodoInput } from "./TodoInput";
 import { createTodoList } from "./TodoList";
 
 export function createTodoApp(): HTMLDivElement {
+    const todos: string[] = [];
+
     const todoInput = createTodoInput((value: string) => {
-        console.log('value', value);
+        todos.unshift(value);
+        const todoList = createTodoList(todos);
+
+        const app = document.querySelector('.todo-app');
+        if (app) {
+            const existingList = app.querySelector('.todo-list');
+            if (existingList) {
+                app.replaceChild(todoList, existingList);
+            } else {
+                app.appendChild(todoList);
+            }
+        }
     });
     
-    const todoList = createTodoList();
-
     const app = document.createElement('div');
+    app.classList.add('todo-app');
     app.appendChild(todoInput);
-    app.appendChild(todoList);
+
 
     return app;
 }
--- src/components/TodoList.ts
+++ src/components/TodoList.ts
@@ -1,5 +1,12 @@
-export function createTodoList(): HTMLUListElement {
-    const ul = document.createElement('ul');
-    ul.id = 'todo-list';
-    return ul;
+export function createTodoList(todos: string[]): HTMLUListElement {
+    const list = document.createElement('ul');
+    list.classList.add('todo-list');
+    
+    todos.forEach(todo => {
+        const todoListItem = document.createElement('li');
+        todoListItem.textContent = todo;
+        list.appendChild(todoListItem);
+    })
+
+    return list;
 }
