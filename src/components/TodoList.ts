ef215b59bd3c24ed1169c5894c8760ec8b4c9380
jennywoon <109018926+jennywoon@users.noreply.github.com>
2025-04-17 22:26:30 +0900

    style: 등록된 Todo 목록 스타일 적용

--- src/components/TodoList.ts
+++ src/components/TodoList.ts
@@ -2,9 +2,17 @@ export function createTodoList(todos: string[]): HTMLUListElement {
     const list = document.createElement('ul');
     list.classList.add('todo-list');
     
+    list.style.listStyleType = 'none';
+    list.style.padding = '0 15px';
+
     todos.forEach(todo => {
         const todoListItem = document.createElement('li');
         todoListItem.textContent = todo;
+
+        todoListItem.style.borderBottom = '1px solid #ddd';
+        todoListItem.style.padding = '10px';
+        todoListItem.style.fontWeight = '600';
+
         list.appendChild(todoListItem);
     })
 
