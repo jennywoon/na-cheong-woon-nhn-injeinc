8da65e580f10ab7561b788980752f90e1149f65f
jennywoon <109018926+jennywoon@users.noreply.github.com>
2025-04-17 21:26:26 +0900

    style: todo 입력 input 생성 및 테두리 스타일 적용

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const input = document.createElement('input');
+input.type = 'text';
+input.placeholder = 'What needs to be done?';
+input.id = 'todo-input';
+
+input.style.padding = '10px';
+input.style.border = '1px dotted #ff3a3a';
+input.style.outline = 'none';
+
+input.addEventListener('focus', () => {
+    input.style.outline = '1px solid #ff3a3a';
+})
+
+input.addEventListener('blur', () => {
+    input.style.outline = 'none';
+})
+
+document.body.appendChild(input);
