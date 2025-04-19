4581946761f5c6275da1e9bcd63254f2443a8c70
jennywoon <109018926+jennywoon@users.noreply.github.com>
2025-04-20 01:41:11 +0900

    chore: Jest에서 CSS 모듈 오류 해결을 위해 styleMock.ts 추가

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,11 @@
-module.exports = {
+export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
     testMatch: ['**/__tests__/**/*.test.ts'],
+    moduleNameMapper: {
+        '\\.css$': '<rootDir>/__mocks__/styleMock.ts',
+    },
 }
